bc1f6ce8df6c4bca97660bbde19cde5c2e1fa38a
hirohata <hirohatatro@gmail.com>
2024-06-06 13:41:09 -0700

    fix skills

--- src/external/fetch.ts
+++ src/external/fetch.ts
@@ -1,8 +1,8 @@
-import { getSdk } from "../gql/request"
-import { GraphQLClient } from 'graphql-request'
+import { GraphQLClient } from "graphql-request";
+import { getSdk } from "../gql/request";
 
 export const fetchResume = async () => {
-	const client = new GraphQLClient("http://localhost:8787/graphql", );
+	const client = new GraphQLClient("http://localhost:8787/graphql");
 	const sdk = getSdk(client);
 	return await sdk.MyResume();
 };
