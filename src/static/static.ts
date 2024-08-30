c6e024226090e771401836fefe9534a7b5e42efc
hirohata <hirohatatro@gmail.com>
2024-08-30 00:01:07 -0700

    format

--- src/static/static.ts
+++ src/static/static.ts
@@ -3,24 +3,26 @@ export const REST_API_URL =
 export const GQL_API_URL =
 	// "https://graphql.honwakapappa-honwakapappa.workers.dev/graphql";
 	"http://localhost:8787/graphql";
-export const RESUME = "https://resume-2dm.pages.dev/pdf/TaroHirohata.pdf"
+export const RESUME = "https://resume-2dm.pages.dev/pdf/TaroHirohata.pdf";
 
-export type DetailNameType = "Resume" | "GraphQL" | "REST API"
+export type DetailNameType = "Resume" | "GraphQL" | "REST API";
 
 type DetailType = {
-	name: DetailNameType,
-	link: string
-}
+	name: DetailNameType;
+	link: string;
+};
 
 export const DETAILS: DetailType[] = [
 	{
 		name: "Resume",
 		link: RESUME,
-	}, {
+	},
+	{
 		name: "GraphQL",
 		link: GQL_API_URL,
-	}, {
+	},
+	{
 		name: "REST API",
 		link: REST_API_URL,
-	}
-]
+	},
+];
