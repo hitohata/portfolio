a0ea7c57336647359682c3b301743e0bbdcc01be
hirohata <hirohatatro@gmail.com>
2024-09-02 00:21:23 -0700

    updated the URL

--- src/static/static.ts
+++ src/static/static.ts
@@ -1,8 +1,7 @@
 export const REST_API_URL =
 	"https://rest-api.honwakapappa-honwakapappa.workers.dev/docs";
 export const GQL_API_URL =
-	// "https://graphql.honwakapappa-honwakapappa.workers.dev/graphql";
-	"http://localhost:8787/graphql";
+	"https://graphql.honwakapappa-honwakapappa.workers.dev/graphql";
 export const RESUME = "https://resume-2dm.pages.dev/pdf/TaroHirohata.pdf";
 
 export type DetailNameType = "Resume" | "GraphQL" | "REST API";
